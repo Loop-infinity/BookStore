8f4ba9ddba061733ea7cd84bc4c280b641deb3a9
Ruman Mulla <rumanmulla911@gmail.com>
2021-01-31 22:02:38 +0000

    Added category model

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BookStore.Models;
 
 namespace BookStore.DataAccess.Data
 {
@@ -12,5 +13,7 @@ namespace BookStore.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { set; get; }
     }
 }
